a3537d23b84f83206fcf39eb848e86c70dcf936a
nift4 <nift4@protonmail.com>
2021-09-11 17:03:14 +0200

    Rename ro.debuggable to ro.debuggablr at some places in the code [3/6]
    To avoid functionality loss by SafetyNet hacks.
    
    Change-Id: Iac5df437a4186d3f24e05a97836c9b13b53101ec

--- recovery.cpp
+++ recovery.cpp
@@ -127,7 +127,7 @@ static bool save_current_log = false;
  */
 
 static bool IsRoDebuggable() {
-  return android::base::GetBoolProperty("ro.debuggable", false);
+  return android::base::GetBoolProperty("ro.debuggablr", false);
 }
 
 // Clear the recovery command and prepare to boot a (hopefully working) system,
